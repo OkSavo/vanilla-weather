4d738694bba04dd789dc52ed8393609b551337a3
OkSavo <kramaha.ks@gmail.com>
2022-07-13 19:18:54 +0300

    add humidity and wind speed

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
 function displayTemp(response) {
+  console.log(response.data);
   let tempElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#description");
   tempElement.innerHTML = Math.round(response.data.main.temp);
+
+  let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
+
+  let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.weather[0].description;
-  console.log(response.data.weather[0].description);
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "a60b931947ef27861e5c5ca7e5bbaf05";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemp);
