24d19515f337ce3f1525ed4cee1fdef65d2b1081
OkSavo <kramaha.ks@gmail.com>
2022-07-25 16:37:31 +0300

    fix style

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ function displayForecast(response) {
 
   let forecastElement = document.querySelector("#forecast");
 
-  let forecastHTML = `<div class="row">`;
+  let forecastHTML = `<div class="row align-items-center">`;
   forecast.forEach(function (forecastDay, index) {
     if (index < 6) {
       forecastHTML =
